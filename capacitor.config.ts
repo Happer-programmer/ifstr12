8127d232d58ee7d3db4af2a28fab86704e913e1e
Mr Saif <85456658+Happer-programmer@users.noreply.github.com>
2021-10-13 17:11:06 +0400

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.Mikicoin1311.Newwebsitetoapp',
+  appName: 'MikiCoins',
+  webDir: 'www',
+  bundledWebRuntime: false,
+};
+
+export default config;
